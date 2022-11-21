ba48f10e2e2e7bf56d37c7e230faf01961d79a57
Андрей Больбот <@in.online1c.com>
2022-11-21 16:47:21 +0200

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongo = require('mongoose')
+const cors = require('cors')
+const conf = require('config')
+
+
+const app = express()
+const PORT = conf.get('port') || 6000
+
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+
+const serverStart = async () => {
+    try {
+        await mongo.connect(conf.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        app.listen( PORT, () => console.log(`Started at port ${PORT}`) )
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+serverStart()
